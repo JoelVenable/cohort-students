b467b2c123236e433a9a9196837d06578c6e564a
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-11 10:21:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cohort_students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
